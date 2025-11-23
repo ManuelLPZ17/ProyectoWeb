255650df0de802cb27134343f14d7d90c96f9145
ManuelLPZ17 <alfonso.lopez@iteso.mx>
2025-11-22 20:12:07 -0600

    Conecte MongDB

--- BACKEND/config/config.js
+++ BACKEND/config/config.js
@@ -3,7 +3,7 @@
 // NOTA: En un proyecto real, DB_HOST se leería desde un archivo .env
 const config = {
     // Reemplaza 'TU_CADENA_DE_CONEXION' con tu URL real de MongoDB Atlas
-    DB_HOST: "mongodb+srv://USUARIO:PASSWORD@cluster0.abcde.mongodb.net/CineclickDB", 
+    DB_HOST: "mongodb+srv://admin:admin123@myapp.zw6q3rt.mongodb.net/MyAppDB",
     PORT: 3000,
     ADMIN_AUTH_KEY: "admin_auth_key_secreta" // Clave para autenticación de administrador
 };
